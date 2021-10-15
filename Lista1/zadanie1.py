4adae511cf68c09bf56575cf158f3d7357462231
DorianSzlachcic <329666@uwr.edu.pl>
2021-10-15 12:46:23 +0200

    gotowe

--- Lista1/zadanie1.py
+++ Lista1/zadanie1.py
@@ -3,5 +3,6 @@ b = input()
 suma = a + b
 print(suma)
 
+
 #Podając do inputu np. liczby 5 i 2, suma będzie wynosiła 52 zamiast 7
 #jest tak dlatego, że funkcja input zapisuje do zmiennej wartość typu str
--- Lista1/zadanie10.py
+++ Lista1/zadanie10.py
@@ -9,4 +9,5 @@ a = (cmath.sin(z)**2) + (cmath.cos(z)**2)
 
 print(a)
 print("Jedynka trygonometryczna spełniona")
+
 #Wszystko działa ;)
